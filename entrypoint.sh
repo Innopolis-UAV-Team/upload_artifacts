2f5ca25c496c576af2bc18bfcf24a62139e1483b
Stepan Kuznetsov <Stepan14511@gmail.com>
2024-12-10 10:13:45 +0300

    Changed for StepanServer

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,15 +13,15 @@ current_time=$(git show -s --date=format:"%Y.%m.%d-%H:%M" --format=%cd)
 commit_backet_name="${current_time}...SHA-${commit_sha}"
 
 # Using lftp to upload files
-mc alias set myminio http://localhost:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
-mc mb myminio/artifacts/$repo_name/$branch_name/$commit_backet_name
-mc cp $path myminio/artifacts/$repo_name/$branch_name/$commit_backet_name/
+~/mc alias set myminio http://localhost:9000 $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+~/mc mb myminio/artifacts/$repo_name/$branch_name/$commit_backet_name
+~/mc cp $path myminio/artifacts/$repo_name/$branch_name/$commit_backet_name/
 
 echo ""
 echo ""
 echo "Artifact has been uploaded successfuly. You can find your file at:"
 echo "----------FILE LOCATION----------"
-echo "VPN (Netherlands):     http://10.8.0.4:9001/browser/artifacts/${repo_name}%2F${branch_name}%2F${commit_backet_name}%2F"
-echo "WIFI (might change):   http://10.95.0.117:9001/browser/artifacts/${repo_name}%2F${branch_name}%2F${commit_backet_name}%2F"
+echo "VPN (Netherlands):     http://10.8.0.19:9001/browser/artifacts/${repo_name}%2F${branch_name}%2F${commit_backet_name}%2F"
+echo "WIFI (might change):   http://10.95.0.140:9001/browser/artifacts/${repo_name}%2F${branch_name}%2F${commit_backet_name}%2F"
 echo "---------------------------------"
 echo "If you have any problems with getting your artifacts, please contact @jpeg_not_ded or @AsiiaBara (if someone is still working here)."
